f59e32c583b29dfe194a86efcc47acdc20ce6b4b
Rajneesh Khare <rajneeshkhare94@gmail.com>
2023-03-20 21:35:04 +0530

    added home

--- banksAPI/urls.py
+++ banksAPI/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from banksAPI.views import bank_list, branch_list, ifsc_details
 
 urlpatterns = [
+	path('', bank_list),
     path('banks/', bank_list),
     path('branches/<str:bank_id>/', branch_list),
     path('ifsc/<str:ifsc_code>/', ifsc_details, name='ifsc-details'),
